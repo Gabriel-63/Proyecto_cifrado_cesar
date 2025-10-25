3df368fb03c1e5ea150905abf5672f8b1641f120
Gabriel <Gabriel@DESKTOP-T8OMP23>
2025-10-24 23:22:40 -0600

    Proyecto version 1

---
+++ src/main/java/com/mycompany/cifrado/Cifrado.java
@@ -0,0 +1,90 @@
+package com.mycompany.cifrado;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class  Cifrado{
+
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+        String op, op1;
+        System.out.println("¿Desea cifrar o descifrar?");
+        System.out.println("Cifrar [C] - Descifrar [D]");
+        op = lector.next().toUpperCase();
+        do {
+            switch (op) {
+                case "C": {
+                    Cifrar();
+                    break;
+                }
+                case "D": {
+                    Descifrar();
+                    break;
+                }
+                default:
+                    System.out.println("Opción no válida ingrese: Cifrar [C] o Descifrar [D] .");
+                    break;
+            }
+            System.out.println("Quiere salir del programa?");
+            op1 = lector.next().toUpperCase();
+            if (op1.equals("NO")) {
+                System.out.println("¿Desea cifrar o descifrar?");
+                System.out.println("Cifrar [C] - Descifrar [D]");
+                op = lector.next().toUpperCase();
+            }
+        } while (op1.equals("NO"));
+    }
+
+    public static void Cifrar() {
+        Scanner lector = new Scanner(System.in);
+        System.out.println("Ingrese el texto que quiere cifrar");
+        String txt = lector.nextLine().toUpperCase();
+        System.out.println("Ingrese la clave para cifrar");
+        int cl = lector.nextInt();
+        StringBuilder txtC = new StringBuilder();
+        for (int i = 0; i < txt.length(); i++) {
+            char c = txt.charAt(i);
+            if (Character.isLetter(c)) {
+                char cif = (char) ((c +(cl-65)) % 26 + 65);
+                txtC.append(cif);
+            }else {
+                txtC.append(c); 
+            }
+        }
+        System.out.println("El texto cifrado es " + txtC);
+        guardado(txt,txtC);
+    }
+
+    public static void Descifrar() {
+        Scanner lector = new Scanner(System.in);
+        System.out.println("Ingrese el texto que quiere descifrar:");
+        String txt = lector.nextLine().toUpperCase();
+        System.out.println("Ingrese la clave para cifrar");
+        int cl = lector.nextInt();
+        StringBuilder txtC = new StringBuilder();
+        for (int i = 0; i < txt.length(); i++) {
+            char c = txt.charAt(i);
+            if (Character.isLetter(c)) {
+                char des = (char) ((c -(cl-65)) % 26 + 65);
+                txtC.append(des);
+            } else {
+                txtC.append(c);
+            }
+        }
+        System.out.println("El texto descifrado es " + txtC);
+        guardado(txt,txtC);
+    }
+    
+    public static void guardado(String original, StringBuilder txtC){
+        try(FileWriter file=new FileWriter("Datos.txt",true)){
+            file.write(original+"   \t"+txtC+"\n");
+        }catch(IOException e){
+            System.out.println("Error al guardar el archivo "+e);
+        }
+            
+    }
+}
